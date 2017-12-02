7b3de35817e2361d97fcf2ff6ae55622609a51aa
katcavers <katcav95@gmail.com>
2017-12-02 13:53:29 -0800

    Using request-promise to make request and get the sentiment value.

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var restify = require('restify');
 var builder = require('botbuilder');
+var https = require('https');
+var rp = require('request-promise');
 
 var MICROSOFT_APP_ID = '2b72e45a-84b0-4f50-8615-e80cdcb7068c';
 var MICROSOFT_APP_PASSWORD = 'oqwrIM3244!@pckKXBJM8?]';
 
-var headers = {'Content-Type':'application/json', 'Ocp-Apim-Subscription-Key':'300e68bffa5449dfb895b55982140de7'}
+var header = {'Content-Type':'application/json', 'Ocp-Apim-Subscription-Key':'300e68bffa5449dfb895b55982140de7'}
 var requestUrl = 'https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment';
 
 // Setup Restify Server
@@ -25,4 +27,26 @@ server.post('/api/messages', connector.listen());
 
 bot.dialog('/', function(session) {
   session.send("You said: %s", session.message.text);
-});
+        sendGetSentimentRequest(session.message.text).then(function (parsedBody) {
+            console.log(parsedBody);
+            session.send(parsedBody.documents[0].score.toString());
+        })
+        .catch(function (err) {
+            console.log("POST FAILED: " + err);
+        });
+
+  });
+
+function sendGetSentimentRequest(message) {
+    var options = {
+        method: 'POST',
+        uri: requestUrl,
+        body: {
+            documents:[{id:'1', language: 'en', text:message}]
+        },
+        json: true, // Automatically stringifies the body to JSON,
+        headers: header
+    };
+    return rp(options);
+
+}
