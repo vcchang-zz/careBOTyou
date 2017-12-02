1a30a0b11512045eb6351b354195adceb5c8d358
Beriwan <beriwans-ravandi@hotmail.com>
2017-12-02 11:45:11 -0800

    fixed mistake

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var restify = require('restify');
 var builder = require('botbuilder');
 
-var MICROSOFT_APP_PASSWORD = 'oqwrIM3244!@pckKXBJM8?]';
 var MICROSOFT_APP_ID = '2b72e45a-84b0-4f50-8615-e80cdcb7068c';
+var MICROSOFT_APP_PASSWORD = 'oqwrIM3244!@pckKXBJM8?]';
 
 // Setup Restify Server
 var server = restify.createServer();
@@ -12,8 +12,8 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.MICROSOFT_APP_PASSWORD
+    appId: MICROSOFT_APP_ID,
+    appPassword: MICROSOFT_APP_PASSWORD
 });
 
 // Listen for messages from users
