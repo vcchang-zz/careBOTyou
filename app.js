de1b348dc5362bd758209c52bff653ad2480fd8f
Beriwan <beriwans-ravandi@hotmail.com>
2017-12-02 12:28:31 -0800

    added API

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var builder = require('botbuilder');
 var MICROSOFT_APP_ID = '2b72e45a-84b0-4f50-8615-e80cdcb7068c';
 var MICROSOFT_APP_PASSWORD = 'oqwrIM3244!@pckKXBJM8?]';
 
+var headers = {'Content-Type':'application/json', 'Ocp-Apim-Subscription-Key':'300e68bffa5449dfb895b55982140de7'}
+var requestUrl = 'https://[location].api.cognitive.microsoft.com/text/analytics/v2.0/sentiment';
+
 // Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
@@ -16,10 +19,10 @@ var connector = new builder.ChatConnector({
     appPassword: MICROSOFT_APP_PASSWORD
 });
 
+var bot = new builder.UniversalBot(connector);
 // Listen for messages from users
 server.post('/api/messages', connector.listen());
 
-// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
-var bot = new builder.UniversalBot(connector, function (session) {
-    session.send("You said: %s", session.message.text);
+bot.dialog('/', function(session) {
+  session.send("You said: %s", session.message.text);
 });
