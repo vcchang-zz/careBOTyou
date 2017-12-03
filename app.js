9e78ef4f09de5fafd16cbaa9225aa119629d3eb6
katcavers <katcav95@gmail.com>
2017-12-02 16:58:57 -0800

    Adding the happy stuff

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var rp = require('request-promise');
 var MICROSOFT_APP_ID = '2b72e45a-84b0-4f50-8615-e80cdcb7068c';
 var MICROSOFT_APP_PASSWORD = 'oqwrIM3244!@pckKXBJM8?]';
 
-var header = {'Content-Type':'application/json', 'Ocp-Apim-Subscription-Key':'300e68bffa5449dfb895b55982140de7'}
+var header = {'Content-Type':'application/json', 'Ocp-Apim-Subscription-Key':'6fb7959510a84e389985fd3343705e6b'}
 var requestUrl = 'https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment';
 
 // Setup Restify Server
@@ -26,16 +26,116 @@ var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
 bot.dialog('/', function(session) {
-  session.send("You said: %s", session.message.text);
         sendGetSentimentRequest(session.message.text).then(function (parsedBody) {
-            console.log(parsedBody);
-            session.send(parsedBody.documents[0].score.toString());
+            var score = parsedBody.documents[0].score;
+            if (score > 0.8) {
+                session.beginDialog('/happy');
+            }
         })
         .catch(function (err) {
             console.log("POST FAILED: " + err);
         });
 
   });
+
+bot.dialog('/happy', [
+    function(session) {
+    builder.Prompts.text(session, "That's awesome! What would make you even happier?");
+    },
+    function(session, results) {
+        getGiphy(results.response).then(function(gif) {
+            // session.send(gif.toString());
+            console.log(JSON.parse(gif).data);
+            session.send({
+                text: "Here you go!",
+                attachments: [
+                    {
+                        contentType: 'image/gif',
+                        contentUrl: JSON.parse(gif).data.images.original.url
+                    }
+                ]
+            });
+        }).catch(function(err)
+        {
+            console.log("Error getting giphy: " + err);
+            session.send({
+                text: "We couldn't find that unfortunately :(",
+                attachments: [
+                    {
+                        contentType: 'image/gif',
+                        contentUrl: 'https://media.giphy.com/media/ToMjGpt4q1nF76cJP9K/giphy.gif',
+                        name: 'Chicken nugz are life'
+                    }
+                ]
+            });
+        }).then(function(idk) {
+            builder.Prompts.text(session, "Would you like to see more?");
+        });
+    },
+    function (session, results) {
+        if (results.response === "Yes" || results.response === "yes") {
+            session.beginDialog('/giphy');
+        } else {
+            session.send("Have a great rest of your day!!!");
+            session.beginDialog("/");
+        }
+    }
+]);
+
+bot.dialog('/giphy', [
+    function(session) {
+        builder.Prompts.text(session, "What would you like to see?");
+    }, function(session, results) {
+        getGiphy(results.response).then(function(gif) {
+            // session.send(gif.toString());
+            console.log(JSON.parse(gif).data);
+            session.send({
+                text: "Here you go!",
+                attachments: [
+                    {
+                        contentType: 'image/gif',
+                        contentUrl: JSON.parse(gif).data.images.original.url
+                    }
+                ]
+            });
+        }).catch(function(err)
+        {
+            console.log("Error getting giphy: " + err);
+            session.send({
+                text: "We couldn't find that unfortunately :(",
+                attachments: [
+                    {
+                        contentType: 'image/gif',
+                        contentUrl: 'https://media.giphy.com/media/ToMjGpt4q1nF76cJP9K/giphy.gif',
+                        name: 'Chicken nugz are life'
+                    }
+                ]
+            });
+        }).then(function(idk) {
+            builder.Prompts.text(session, "Would you like to see more?");
+        });
+    },
+    function (session, results) {
+        if (results.response === "Yes" || results.response === "yes") {
+            session.beginDialog('/giphy');
+        } else {
+            session.send("Have a great rest of your day!!!");
+            session.beginDialog("/");
+        }
+    }
+]);
+
+function getGiphy(searchString) {
+    var options = {
+        method: 'GET',
+        uri: 'https://api.giphy.com/v1/gifs/translate',
+        qs: {
+            s: searchString,
+            api_key: '9n8AIaWULVu37sA1k8eE38IwnCCjmXP9'
+        }
+    }
+    return rp(options);
+}
 
 function sendGetSentimentRequest(message) {
     var options = {
