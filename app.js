7207c8e7c71a773f730a3dc8fda4ce264ed9edaf
Beriwan <beriwans-ravandi@hotmail.com>
2017-12-02 16:26:32 -0800

    regenerated key

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var rp = require('request-promise');
 var MICROSOFT_APP_ID = '2b72e45a-84b0-4f50-8615-e80cdcb7068c';
 var MICROSOFT_APP_PASSWORD = 'oqwrIM3244!@pckKXBJM8?]';
 
-var header = {'Content-Type':'application/json', 'Ocp-Apim-Subscription-Key':'300e68bffa5449dfb895b55982140de7'}
+var header = {'Content-Type':'application/json', 'Ocp-Apim-Subscription-Key':'6fb7959510a84e389985fd3343705e6b'}
 var requestUrl = 'https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment';
 
 // Setup Restify Server
